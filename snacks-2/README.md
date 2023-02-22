30b7147fd0f0051e310ffecc87f2d504582c63b4
Cristian Cosenza <me@cristiancosenza.com>
2023-02-22 16:27:36 +0100

    feat: snacks-2

--- snacks-2/README.md
+++ snacks-2/README.md
@@ -2,4 +2,9 @@
 
 ---
 
-
+1. Mostriamo due input all'utente
+2. Entrambi i prompt chiederanno all'utente di inserire due parole
+3. Salviamo queste due parole in due variabili differenti
+4. Controllare la lunghezza delle due parole
+5. Stampiamo prima la parola più corta
+6. Infine, stampiamo quella più lunga
--- snacks-2/src/js/scripts.js
+++ snacks-2/src/js/scripts.js
@@ -1,1 +1,22 @@
+/*
+ JSnack 2
+ L’utente inserisce due parole in successione, con due prompt.
+ Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
 
+const parola1 = prompt('Inserisci una parola');
+const parola2 = prompt('Inserisci una parola');
+
+console.log('Parola inserite dall\'utente:', parola1, parola2);
+console.log('La parola1 è lunga ' + parola1.length);
+console.log('La parola2 è lunga ' + parola2.length);
+
+if (parola1.length === parola2.length) {
+    console.log('Il controllo non può essere effettuato perché le parole sono di uguale misura.');
+}
+else if (parola1.length < parola2.length) {
+    console.log(parola1, parola2);
+}
+else {
+    console.log(parola2, parola1);
+}
